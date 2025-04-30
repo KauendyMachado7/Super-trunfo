1b684dfa17724544bdee1bad642f661b2301a554
KauendyMachado7 <kauendydemiranda@gmail.com>
2025-04-29 23:10:00 -0300

    Create Super trunfo.c

---
+++ Super trunfo.c
@@ -0,0 +1,21 @@
+int main(){
+    char estado;
+    char código;
+    char cidade
+    char[50] população; 
+    float área;
+    int pib;
+    int pontosturísticos;
+  
+    estado = Paraná;
+    código A001;
+    cidade Curitiba;
+    população = 1.774; 
+    área 432;
+    pib = 98;
+    pontosturísticos = 35;
+  
+  printf("Digite o Código da carta: \n", código);
+  printf("Estado: \nCódigo: \nCidade: \nPopulação: milões de habitantes. \nÁrea: km². \nPIB: bilhões. \nNúmero de pontotos turísticos: ", estado, código, cidade, população, área, pib, pontosturísticos);
+
+  return 0;
