63c2fd21074a7084ec744d195ca59f6961e1c6fe
KauendyMachado7 <kauendydemiranda@gmail.com>
2025-04-30 19:58:16 -0300

    Update Super trunfo.c

--- Super trunfo.c
+++ Super trunfo.c
@@ -1,21 +1,23 @@
-int main(){
-    char estado;
-    char código;
-    char cidade
-    char[50] população; 
-    float área;
-    int pib;
-    int pontosturísticos;
-  
-    estado = Paraná;
-    código A001;
-    cidade Curitiba;
-    população = 1.774; 
-    área 432;
-    pib = 98;
-    pontosturísticos = 35;
-  
-  printf("Digite o Código da carta: \n", código);
-  printf("Estado: \nCódigo: \nCidade: \nPopulação: milões de habitantes. \nÁrea: km². \nPIB: bilhões. \nNúmero de pontotos turísticos: ", estado, código, cidade, população, área, pib, pontosturísticos);
+#include <stdio.h>
+#include <string.h>
 
-  return 0;
+int main() {
+    char codigo[10];
+
+    printf("Digite o código da carta: ");
+    scanf("%s", codigo);
+
+    if (strcmp(codigo, "A001") == 0) {
+        printf("Código da carta: A001\n");
+        printf("Estado: Paraná\n");
+        printf("Cidade: Curitiba\n");
+        printf("População: 1,774 milhão de habitantes\n");
+        printf("Área: 432 km²\n");
+        printf("PIB: R$98 bilhões\n");
+        printf("Número de pontos turísticos: 35\n");
+    } else {
+        printf("Código.\n");
+    }
+
+    return 0;
+}
